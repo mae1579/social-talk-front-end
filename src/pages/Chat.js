c3bd5c9cf1007127ec31f58a8b05ec17d2217aaf
Mateusz Umięcki <mateuszumiecki@gmail.com>
2022-07-14 19:48:56 +0200

    Creating, configure first pages to application and add to them css files. Added login, register, chat, home, side-bar(front list available friends to chat ) : front-end and logics to send request to back-end from pages.

---
+++ src/pages/Chat.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {Col, Container, Row} from "react-bootstrap";
+import Sidebar from "../components/Sidebar";
+import MessageForm from "../components/MessageForm";
+
+function Chat() {
+    return (
+        <Container style={{backgroundColor: "#FFC600"}}>
+            <Row>
+                <Col md={4}>
+                    <Sidebar/>
+                </Col>
+                <Col md={8}>
+                    <MessageForm/>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+export default Chat;
+
